d7259ebb33568293896015a14346f2b22c73758a
Sofia Prado <sofia.prado.fuentes@gmail.com>
2019-11-05 10:40:32 -0600

    First commit

---
+++ lib/island_engine/application.ex
@@ -0,0 +1,22 @@
+defmodule IslandEngine.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      {Registry, keys: :unique, name: Registry.Game},
+      IslandsEngine.GameSupervisor
+      # Starts a worker by calling: IslandEngine.Worker.start_link(arg)
+      # {IslandEngine.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: IslandEngine.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
